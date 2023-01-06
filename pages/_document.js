814df26e01969f77007b76a338e05d9957240034
Vivek Kumar <vivekr4400@gmail.com>
2023-01-06 16:14:58 +0530

    changes in google task manager

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <script
+                <Script strategy="afterInteractive"
                     dangerouslySetInnerHTML={{
                         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({‘gtm.start’:
                           new Date().getTime(),event:‘gtm.js’});var f=d.getElementsByTagName(s)[0],
