e11275f094eca7b860582583ebf5e66fb377dbd5
Vivek Kumar <vivekr4400@gmail.com>
2023-01-30 20:48:20 +0530

    responsive page

--- components/signup.js
+++ components/signup.js
@@ -6,7 +6,7 @@ import styles from "../styles/signup.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
  
-import Link from "next/link"
+import Link from "next/link" 
 import Head from "next/head";
 
 const signup = ({show,set,showsignup,setsignup}) => {
@@ -123,7 +123,7 @@ return (
  <Head> 
  <title>SignIn Page</title>
 </Head>
-   { show ? <div className={styles.signin_box} id={styles.center}>
+   { show ? <div className={styles.signin_box} id={styles.center}  style={{position:"fixed"}}>
      <p className={styles.mainText}>Sign Up</p>
      <FontAwesomeIcon className={styles.crossIcon} icon={faXmark} onClick={()=>setsignup(false)} /> 
      <div className={styles.container} id={styles.center}>
