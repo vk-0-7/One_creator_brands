0701e6309c520a15e1c4013554ca60a85f30987d
Vivek Kumar <vivekr4400@gmail.com>
2023-03-04 12:21:59 +0530

    issue fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 
-import {Provider} from 'react-redux'
-import store from './redux/store'
+// import {Provider} from 'react-redux'
+// import store from './redux/store'
 
 import React, { useEffect ,useState} from 'react';
 import '../styles/globals.css'
@@ -14,9 +14,9 @@ function App({ Component, pageProps }) {
 
   
   return(<>
-   <Provider store={store}>
+   {/* <Provider > */}
             <Component {...pageProps} />
-        </Provider>
+        {/* </Provider> */}
     </>
     )
 }
